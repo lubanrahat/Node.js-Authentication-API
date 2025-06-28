4d95302f8bd0f7e68b518580da57af1d1a921821
Luban Rahat <lubanrahat.dev@gmail.com>
2025-06-28 19:03:56 +0600

    setup express server

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Node.js-Authentication-API");
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+const port = process.env.PORT;
+
+app.get('/',(req,res)=>{
+    res.send("Hello world!");
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on http://localhost:${port}`);
+});
